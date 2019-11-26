8e6f8592fb7c6bdc00ce7d948daf3be6d05bb753
shanjingzi <hgwang1011@163.com>
2019-11-26 14:02:19 +0800

    add container

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './index.module.scss'
+
+export default class Container extends React.Component<any, {}>{
+
+    constructor(props){
+        super(props)
+    }
+
+    render() {
+        return <div className={styles.container}>
+wertwertewr
+        </div>
+    }
+}
